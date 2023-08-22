b16eb47023be4f96f21d217414c3b5460b8740cd
LucasRocha3 <lucasdelgadorocha@gmail.com>
2023-08-22 08:34:50 -0300

    TI2Teste

---
+++ Aeds 2/Nivelamento/Introdução ao java/TI2teste.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TI2teste {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int x,y;
+		x=input.nextInt();
+		y=input.nextInt();
+		System.out.println(x+y);
+	}
+
+}
