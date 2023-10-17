f773ddd9a6758bdf7176f978bffcdc86b02f8822
LucasRocha3 <lucasdelgadorocha@gmail.com>
2023-10-16 21:15:12 -0300

    o

---
+++ Aeds 2/TP2/TP02Q011.java
@@ -0,0 +1,271 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileWriter;
+
+
+class Jogador {
+
+    private int id;
+    private String nome;
+    private int altura;
+    private int peso;
+    private String universidade;
+    private int anoNascimento;
+    private String cidadeNascimento;
+    private String estadoNascimento;
+
+    public Jogador() {
+        id = 0; 
+        altura = 0;
+        peso = 0;
+        anoNascimento = 0;
+        nome = "";
+        universidade = "";
+        cidadeNascimento = "";
+        estadoNascimento = "";
+    }
+
+    public Jogador(int id, String nome, int altura, int peso, String universidade, int anoNascimento, String cidadeNascimento, String estadoNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+        this.universidade = universidade;
+        this.anoNascimento = anoNascimento;
+        this.cidadeNascimento = cidadeNascimento;
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+   public void setUniversidade(String universidade) {
+        this.universidade = universidade;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public void setCidadeNascimento(String cidadeNascimento) {
+        this.cidadeNascimento = cidadeNascimento;
+    }
+
+    public void setEstadoNascimento(String estadoNascimento) {
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+    
+    public String getUniversidade() {
+        return universidade;
+    }
+
+     public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public String getCidadeNascimento() {
+        return cidadeNascimento;
+    }
+
+    public String getEstadoNascimento() {
+        return estadoNascimento;
+    }
+
+    public void imprimirDados() {
+        System.out.println("[" + getId() + " ## " + getNome() + " ## " + getAltura() + " ## " 
+        + getPeso() + " ## " + getAnoNascimento() + " ## " + getUniversidade() + " ## " + getCidadeNascimento() + " ## " + getEstadoNascimento() + "]");
+    }
+
+    public Jogador clone() {
+        Jogador clone = new Jogador(getId(), getNome(), getAltura(), getPeso(), getUniversidade(), getAnoNascimento(), getCidadeNascimento(), getEstadoNascimento());
+        return clone;
+    }
+
+    public void lerDados(String dadosJogador) {
+        String[] posicao = new String[8];
+        int tmp = 0;
+        int j = 0;
+        for (int i = 0; i < dadosJogador.length(); i++) {
+            if (dadosJogador.charAt(i) == ',') {
+                posicao[j] = dadosJogador.substring(tmp, i);
+                tmp = i + 1;
+                if (posicao[j].intern() == "") {
+                    posicao[j] = "nao informado";
+                }
+                j++;
+            }
+        }
+        posicao[7] = dadosJogador.substring(tmp, dadosJogador.length());
+
+        if (posicao[7] == "") {
+            posicao[7] = "nao informado";
+        }
+        setId(Integer.parseInt(posicao[0]));
+        setNome(posicao[1]);
+        setAltura(Integer.parseInt(posicao[2]));
+        setPeso(Integer.parseInt(posicao[3]));
+        setUniversidade(posicao[4]);
+        setAnoNascimento(Integer.parseInt(posicao[5]));
+        setCidadeNascimento(posicao[6]);
+        setEstadoNascimento(posicao[7]);
+    }
+}
+
+public class TP02Q011 {
+    public static void main(String[] args) {
+        // Obter o tempo inicial para medir o desempenho
+        long tempoInicial = System.currentTimeMillis();
+        
+        // Inicializar uma lista de jogadores
+        ArrayList<Jogador> listaJogadores = new ArrayList<>();
+        
+        try {
+            // Ler dados do arquivo players.csv
+            FileReader leitorArquivo = new FileReader("/tmp/playersAtualizado.csv");
+            BufferedReader arquivo = new BufferedReader(leitorArquivo);
+            arquivo.readLine(); // Ignorar cabeçalho do arquivo
+            while (arquivo.ready()) {
+                Jogador jogador = new Jogador();
+                jogador.lerDados(arquivo.readLine());
+                listaJogadores.add(jogador);
+            }
+            arquivo.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage() + "\n" + ex.getLocalizedMessage());
+        }
+        
+        // Inicializar uma lista para armazenar jogadores selecionados pelo usuário
+        ArrayList<Jogador> jogadoresSelecionados = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.nextLine();
+        
+        // Ler IDs dos jogadores fornecidos pelo usuário até que "FIM" seja inserido
+        while (!id.equals("FIM")) {
+            int n = Integer.parseInt(id);
+            if (n >= 0 && n < listaJogadores.size()) {
+                jogadoresSelecionados.add(listaJogadores.get(n));
+            } else {
+                System.out.println("Índice inválido, insira um índice válido ou 'FIM' para encerrar.");
+            }
+            id = scanner.nextLine();
+        }
+        scanner.close();
+        
+        // Realizar a ordenação usando o algoritmo Counting Sort e contar comparações e movimentações
+        int[] contador = countingSort(jogadoresSelecionados);
+        
+        // Obter o tempo final e calcular o tempo total
+        long tempoFinal = System.currentTimeMillis();
+        
+        try {
+            // Escrever os resultados no arquivo matricula_countingsort.txt
+            FileWriter escritorArquivo = new FileWriter("matricula_countingsort.txt");
+            BufferedWriter arqWriter = new BufferedWriter(escritorArquivo);
+            arqWriter.write("Matrícula: 801434\tTempo: " + (tempoFinal - tempoInicial) / 1000d +
+                    "\tComparações: " + contador[0] + "\tMovimentações: " + contador[1]);
+            arqWriter.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage() + "\n" + ex.getLocalizedMessage());
+        }
+    }
+    
+    public static int getMaiorAltura(ArrayList<Jogador> array) {
+        int maiorAltura = array.get(0).getAltura();
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i).getAltura() > maiorAltura)
+                maiorAltura = array.get(i).getAltura();
+        }
+        return maiorAltura;
+    }
+    
+    public static int[] countingSort(ArrayList<Jogador> array1) {
+        // Inicializar contadores para comparações e movimentações
+        int comparacoes = 0;
+        int movimentacoes = 0;
+    
+        // Inicializar arrays auxiliares
+        int[] indicesOrdenados = new int[array1.size()];
+        int[] contadorAlturas = new int[getMaiorAltura(array1)];
+        ArrayList<Jogador> ordenado = new ArrayList<>();
+    
+        // Contar a frequência de cada altura na lista de jogadores
+        for (int i = 0; i < array1.size(); i++) {
+            contadorAlturas[array1.get(i).getAltura() - 1] += 1;
+        }
+    
+        // Calcular as posições finais de cada altura na lista ordenada
+        for (int i = 1; i < contadorAlturas.length; i++) {
+            contadorAlturas[i] += contadorAlturas[i - 1];
+        }
+    
+        // Mapear jogadores para as posições corretas na lista ordenada usando contadorAlturas
+        for (int i = array1.size() - 1; i >= 0; i--) {
+            indicesOrdenados[contadorAlturas[array1.get(i).getAltura() - 1] - 1] = i;
+            contadorAlturas[array1.get(i).getAltura() - 1] -= 1;
+        }
+    
+        // Mover jogadores ordenados de array1 para a lista ordenada, contar movimentações
+        for (int i = 0; i < indicesOrdenados.length; i++) {
+            ordenado.add(array1.get(indicesOrdenados[i]));
+            movimentacoes++;
+        }
+    
+        // Comparar e ordenar jogadores com a mesma altura por nome usando o método de seleção
+        for (int i = 0; i < ordenado.size(); i++) {
+            for (int j = i + 1; j < ordenado.size(); j++) {
+                comparacoes++;
+                if (ordenado.get(i).getAltura() == ordenado.get(j).getAltura()) {
+                    comparacoes++;
+                    if (ordenado.get(i).getNome().compareToIgnoreCase(ordenado.get(j).getNome()) > 0) {
+                        comparacoes++;
+                        Jogador temp = ordenado.get(i);
+                        ordenado.set(i, ordenado.get(j));
+                        ordenado.set(j, temp);
+                        movimentacoes += 3;
+                    }
+                }
+            }
+        }
+    
+        // Imprimir dados ordenados (opcional, dependendo dos requisitos)
+        for (int i = 0; i < ordenado.size(); i++) {
+            ordenado.get(i).imprimirDados();
+        }
+    
+        // Retornar o número de comparações e movimentações
+        return new int[]{comparacoes, movimentacoes};
+    }
+    
+}
---
+++ Aeds 2/TP2/TP02Q10.c
@@ -0,0 +1,262 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Definição da estrutura
+typedef struct Jogador{
+    int id;
+    char nome[40];
+    int altura;
+    int peso;
+    char universidade[100];
+    int anoNascimento;
+    char cidadeNascimento[100];
+    char estadoNascimento[100];
+
+}  Jogador;
+
+    void setID(Jogador *p, int id){
+        p->id = id;
+    }
+
+    void setNome(Jogador *p, char nome[]){
+        strcpy(p->nome, nome);
+    }
+
+    void setAltura(Jogador *p, int alt){
+        p->altura = alt;
+    }
+
+    void setPeso(Jogador *p, int peso){
+        p->peso = peso;
+    }
+
+    void setUniversidade(Jogador *p, char texto[]){
+        strcpy(p->universidade, texto);
+    }
+
+    void setAno(Jogador *p, int ano){
+        p->anoNascimento = ano;
+    }
+
+    void setCidade(Jogador *p, char cidade[]){
+        strcpy(p->cidadeNascimento,cidade);
+    }
+
+    void setEstado(Jogador *p, char estado[]){
+        strcpy(p->estadoNascimento,estado);
+    }
+
+    int getID(Jogador *p){
+        return p->id;
+    }
+
+    char* getNome(Jogador *p){
+        return p->nome;
+    }
+
+    int getAltura(Jogador *p){
+        return p->altura;
+    }
+
+    int getPeso(Jogador *p){
+        return p->peso;
+    }
+
+    char* getUniversidade(Jogador *p){
+        return p->universidade;
+    }
+
+    int getAno(Jogador *p){
+        return p->anoNascimento;
+    }
+
+    char* getCidade(Jogador *p){
+        return p->cidadeNascimento;
+    }
+
+    char* getEstadoNascimento(Jogador *p){
+        return p->estadoNascimento;
+    }
+
+    void ler(Jogador *jogador, char linha[1000]){
+        char id[10];
+        char nome[40];
+        char altura[40];            
+        char peso[40];
+        char universidade[100];
+        char anoNascimento[40];
+        char cidadeNascimento[100];
+        char estadoNascimento[100];
+        int virgulas[7];
+        int contVirgula;
+
+        //guardando em um vetor a pos das vírgulas
+        contVirgula = 0;
+        for(int i = 0; i < strlen(linha); i++){
+            if(linha[i] == ','){
+                virgulas[contVirgula++] = i;               
+            }
+        }
+        //for da id
+        int j;
+        for(j = 0; j < virgulas[0]; j++){
+            id[j] = linha[j];
+        }
+        id[++j] = '\0';
+        int ID = atoi(id);
+        setID(jogador, ID);
+
+        //for do nome
+        int k = 0;
+        for(j = virgulas[0] + 1; j < virgulas[1]; j++){
+            nome[k++] = linha[j];
+        }
+        nome[k] = '\0';
+        k = 0;
+        setNome(jogador, nome);
+
+        // Extraindo a altura
+        k = 0;
+        for (j = virgulas[1] + 1; j < virgulas[2]; j++) {
+            altura[k++] = linha[j];
+        }
+        altura[k] = '\0';
+        int Altura = atoi(altura);
+        setAltura(jogador, Altura);
+
+        // Extraindo o peso
+        k = 0;
+        for (j = virgulas[2] + 1; j < virgulas[3]; j++) {
+            peso[k++] = linha[j];
+        }
+        peso[k] = '\0';
+        int Peso = atoi(peso);  
+        setPeso(jogador, Peso);     
+
+        // Extraindo a universidade
+        k = 0;
+        if(virgulas[4] - virgulas[3] == 1){
+            strcpy(universidade, "nao informado\0");
+        }
+        else{
+            for (j = virgulas[3] + 1; j < virgulas[4]; j++) {
+                universidade[k++] = linha[j];
+            }
+            universidade[k] = '\0';
+        }   
+        setUniversidade(jogador, universidade);  
+
+        // Extraindo o ano de nascimento
+        k = 0;
+        for (j = virgulas[4] + 1; j < virgulas[5]; j++) {
+            anoNascimento[k++] = linha[j];
+        }
+        anoNascimento[k] = '\0';
+        int ano = atoi(anoNascimento);
+        setAno(jogador, ano);
+
+        // Extraindo a cidade de nascimento
+        k = 0;
+        if(virgulas[6] - virgulas[5] == 1){
+            strcpy(cidadeNascimento, "nao informado\0");
+        }
+        else{
+            for (j = virgulas[5] + 1; j < virgulas[6]; j++) {
+                cidadeNascimento[k++] = linha[j];
+            }
+            cidadeNascimento[k] = '\0';
+        }
+        setCidade(jogador, cidadeNascimento);
+
+        // Extraindo o estado de nascimento
+        k = 0;
+        if(strlen(linha) - 1 - virgulas[6] == 1){
+            strcpy(estadoNascimento, "nao informado\0");
+        }
+        else{
+            for (j = virgulas[6] + 1; j < strlen(linha) - 1; j++) {
+                estadoNascimento[k++] = linha[j];
+            }
+            estadoNascimento[k] = '\0';
+        }
+        setEstado(jogador, estadoNascimento);
+
+    }
+    void imprimir(Jogador jogador){
+        char* estado = getEstadoNascimento(&jogador);
+        int estado_len = strlen(estado);
+
+        if (estado_len > 0 && estado[estado_len - 1] == ',') {
+            estado[estado_len - 1] = '\0';  // Substitui a vírgula pelo caractere nulo para remover.
+        }
+        printf("[%d ## %s ## %d ## %d ## %d ## %s ## %s ## %s]\n", getID(&jogador), getNome(&jogador), getAltura(&jogador), getPeso(&jogador), getAno(&jogador), getUniversidade(&jogador), getCidade(&jogador), getEstadoNascimento(&jogador));
+
+    }
+    void swap(Jogador* x, Jogador* y){
+        Jogador tmp = *y;
+        *y = *x;
+        *x = tmp;
+    }
+    void quicksort(Jogador jogadorID[], int esq, int dir){
+        int i = esq, j = dir;
+        Jogador pivo = jogadorID[(esq + dir) / 2];
+        while(i <= j){
+            //caso o estado de nascimento de um esteja antes na ordem alfabética ou caso seja igual o nome de um esteja na frente do outro
+            while(strcmp(getEstadoNascimento(&jogadorID[i]), getEstadoNascimento(&pivo)) < 0 ||(strcmp(getEstadoNascimento(&jogadorID[i]), getEstadoNascimento(&pivo)) == 0 && strcmp(getNome(&jogadorID[i]), getNome(&pivo)) < 0)) i++;
+
+            while(strcmp(getEstadoNascimento(&jogadorID[j]), getEstadoNascimento(&pivo)) > 0 ||(strcmp(getEstadoNascimento(&jogadorID[j]), getEstadoNascimento(&pivo)) == 0 && strcmp(getNome(&jogadorID[j]), getNome(&pivo)) > 0))j--;
+
+
+            if(i <= j){
+                swap(&jogadorID[i], &jogadorID[j]);
+                i++;
+                j--;
+            }
+        }
+        if (esq < j)  quicksort(jogadorID, esq, j);
+        if (i < dir)  quicksort(jogadorID, i, dir);
+    }
+
+
+int main(void) {
+    Jogador jogador[4000];
+    
+    FILE *arq;
+    arq = fopen("/tmp/playersAtualizado.csv", "r, ccs=UTF-8");
+    char linha[1000];
+    int virgulas[7];
+    int contVirgula;
+    //descarte primeira linha
+    fgets(linha, 1000, arq);
+    int cont = 0;
+
+    //lendo todas as linhas do arquivo
+    while(fgets(linha, 1000, arq)){       
+        ler(&jogador[cont++], linha);
+
+    }
+
+    //pegando os id de entrada e colocando em um novo vetor de objetos
+    Jogador jogadorID[4000];
+    int controle = 0;
+    char entrada[5];
+    scanf(" %s", entrada);
+    while(strcmp(entrada, "FIM") != 0){
+        int ent = atoi(entrada);
+        for(int i = 0; i <= 3921; i++){
+            if(getID(&jogador[i]) == ent){
+                jogadorID[controle++] = jogador[i];
+                break;
+            }
+        }
+        scanf("%s", entrada);
+    }
+    quicksort(jogadorID, 0, controle);
+
+    for(int i = 1; i < controle; i++){
+        imprimir(jogadorID[i]);
+    }
+
+    return 0;
+}
